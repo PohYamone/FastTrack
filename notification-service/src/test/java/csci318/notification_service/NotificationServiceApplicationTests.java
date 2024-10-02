888805d6d3cf3ffd2e548287f5385c271e58a99e
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-10-02 17:46:13 +1000

    updating POMS v2

---
+++ notification-service/src/test/java/csci318/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package csci318.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
