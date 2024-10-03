e32453e16ef74589422839902d32fce2bee1e592
PohSA <pohyamone18@gmail.com>
2024-10-03 16:35:35 +1000

    t

--- inventory-service/src/main/java/csci318/inventory_service/Model/Event/Inventory.java
+++ inventory-service/src/main/java/csci318/inventory_service/Model/Event/Inventory.java
@@ -1,15 +1,23 @@
 package csci318.inventory_service.Model.Event;
 
 import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "Inventory")
 public class Inventory {
 
     @Id
+    @Column(name = "product_id")
     private String productId;
+
+    @Column(name = "product_name")
     private String productName;
+
+    @Column(name = "available_stock")
     private int availableStock;
-
-
     public Inventory(){
 
     }
@@ -24,21 +32,29 @@ public class Inventory {
     public void setProductId(String productId){
         this.productId = productId;
     }
-
     public String getProductId(){
         return productId;
     }
 
-    public void setProductName(){
+
+
+    public void setProductName(String productName){
         this.productName = productName;
     }
     public String getProductName(){
         return productName;
     }
+
+
+
     public void setAvailableQuantity(int availableStock) {
         this.availableStock = availableStock;
     }
+    public int getAvailableQuantity(int availableStock) {
+        return availableStock;
+    }
    
 }
+
 
 
