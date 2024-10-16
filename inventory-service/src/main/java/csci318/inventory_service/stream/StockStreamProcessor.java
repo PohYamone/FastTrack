6a393971cbfd9b31002966dffc6f9673a9a06bdc
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-10-16 19:49:47 +1100

    remove dependencies

--- inventory-service/src/main/java/csci318/inventory_service/stream/StockStreamProcessor.java
+++ inventory-service/src/main/java/csci318/inventory_service/stream/StockStreamProcessor.java
@@ -1,26 +1,18 @@
 package csci318.inventory_service.stream;
 
-import java.util.Arrays;
 import java.util.function.Consumer;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Printed;
-import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
-import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
-import org.springframework.kafka.core.KafkaTemplate;
 
 import csci318.inventory_service.Model.Event.StockEvent;
 
@@ -28,7 +20,7 @@ import csci318.inventory_service.Model.Event.StockEvent;
 @Configuration
 public class StockStreamProcessor {
 
-    public static final String TOTAL_STOCK = "total-stock";
+    public static final String TOTAL_STOCK = "totalStock";
     private static final int LOW_STOCK_THRESHOLD = 5;
     
     @Bean
@@ -59,6 +51,5 @@ public class StockStreamProcessor {
 
     private void triggerLowStockAlert(Long productId, Integer stockLevel) {
         System.out.println("ALERT: Product " + productId + " has low stock: " + stockLevel);
-        //yo we maybe add a alert to nitifications here???
     }
 }
