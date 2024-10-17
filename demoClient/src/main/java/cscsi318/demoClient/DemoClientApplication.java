56ff44a65d4ad3017b294bfd385cded63e2a2d14
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-10-17 17:42:00 +1100

    update add demoCLient

---
+++ demoClient/src/main/java/cscsi318/demoClient/DemoClientApplication.java
@@ -0,0 +1,13 @@
+package cscsi318.demoClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoClientApplication.class, args);
+	}
+
+}
